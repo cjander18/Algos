07e2d8d7e17e4e82a402970f5e565d60e49b263b
Clinton Anderson <Clinton Anderson>
2016-06-12 10:25:41 -0400

    new file:   .vs/Algorithms/v14/.suo 	new file:   Algorithms.sln 	new file:   Algorithms/Algorithms.csproj 	new file:   Algorithms/App.config 	new file:   Algorithms/InsertionSort.cs 	new file:   Algorithms/Program.cs 	new file:   Algorithms/Properties/AssemblyInfo.cs 	new file:   Algorithms/bin/x64/Debug/Algorithms.exe 	new file:   Algorithms/bin/x64/Debug/Algorithms.exe.config 	new file:   Algorithms/bin/x64/Debug/Algorithms.pdb 	new file:   Algorithms/bin/x64/Debug/Algorithms.vshost.exe 	new file:   Algorithms/bin/x64/Debug/Algorithms.vshost.exe.config 	new file:   Algorithms/bin/x64/Release/Algorithms.exe 	new file:   Algorithms/bin/x64/Release/Algorithms.exe.config 	new file:   Algorithms/bin/x64/Release/Algorithms.pdb 	new file:   Algorithms/bin/x64/Release/Algorithms.vshost.exe 	new file:   Algorithms/bin/x64/Release/Algorithms.vshost.exe.config 	new file:   Algorithms/bin/x64/Release/Algorithms.vshost.exe.manifest 	new file:   Algorithms/obj/x64/Debug/Algorithms.csproj.FileListAbsolute.txt 	new file:   Algorithms/obj/x64/Debug/Algorithms.exe 	new file:   README.md 	modified:   Algorithms/Algorithms.csproj 	modified:   Algorithms/Program.cs 	modified:   Algorithms/bin/x64/Debug/Algorithms.exe 	modified:   Algorithms/bin/x64/Debug/Algorithms.pdb 	modified:   Algorithms/bin/x64/Release/Algorithms.exe 	modified:   Algorithms/bin/x64/Release/Algorithms.pdb 	deleted:    Algorithms/bin/x64/Release/Algorithms.vshost.exe.manifest 	modified:   Algorithms/obj/x64/Debug/Algorithms.csproj.FileListAbsolute.txt 	Algorithms/MergeSort.cs 	Algorithms/bin/x64/Debug/Algorithms.vshost.exe.manifest 	Algorithms/obj/x64/Debug/Algorithms.csprojResolveAssemblyReference.cache 	Algorithms/obj/x64/Debug/DesignTimeResolveAssemblyReferencesInput.cache 	Algorithms/obj/x64/Release/Algorithms.csprojResolveAssemblyReference.cache

---
+++ Algorithms/InsertionSort.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+
+{
+
+    class InsertionSort
+
+    {
+
+        private Random rand = new Random();
+
+        public int[] intArrRandom { get; set; }
+
+        // Initalize the integer array with random values
+        public InsertionSort()
+
+        {
+
+            // Create an array with a random length
+            intArrRandom = new int[rand.Next(0, 100)];
+
+            // For each place in the array
+            for (int i = 0; i < intArrRandom.Count(); i++)
+
+            {
+                // Put a random number in it
+                intArrRandom[i] = rand.Next(0, 100);
+            }
+
+        }
+
+        // Sort the array
+        public void SortArray()
+
+        {
+
+            // For each place in the array
+            for (int intCurrentLocation = 0; intCurrentLocation < intArrRandom.Count(); intCurrentLocation++)
+
+            {
+
+                // Get the value of the current location
+                int intCurrentValue = intArrRandom[intCurrentLocation];
+
+                // Get the prior location
+                int intPriorLocation = intCurrentLocation - 1;
+
+                // If the prior location is the first or greater positon
+                // And the prior location's value is greater than the current value
+                while (intPriorLocation >= 0 && intArrRandom[intPriorLocation] > intCurrentValue)
+
+                {
+
+                    // Then we need to move the prior value up one, since it's greater than our current value
+                    intArrRandom[intPriorLocation + 1] = intArrRandom[intPriorLocation];
+
+                    // And move back down the list, looking for a value smaller than our current value
+                    intPriorLocation = intPriorLocation - 1;
+
+                }
+
+                // Once we've either come to the end of the list or found the current values proper location
+                intArrRandom[intPriorLocation + 1] = intCurrentValue;
+
+            }
+
+        }
+
+    }
+
+}
